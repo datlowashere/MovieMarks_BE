f92d0e2ff69d9be39d649bf80d0359a0d9c42ae2
Đạt <datlowashere@gmail.com>
2025-02-07 23:20:09 +0700

    config app

--- src/config/mongoosedb.js
+++ src/config/mongoosedb.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 async function connect() {
   try {
     await mongoose.connect(process.env.MONGO_URI);
+    
     console.log("Connected!");
   } catch (error) {
     console.log("Failled" + error);
